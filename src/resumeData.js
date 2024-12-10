c8212754ca6812a597f0d108ad6a44dfa965a6ba
Mohamed Shahin <mshahin@innotech.com.eg>
2024-12-10 19:07:00 +0200

    Create Portfolio

---
+++ src/resumeData.js
@@ -0,0 +1,90 @@
+let resumeData = {
+  imagebaseurl: "https://rbhatia46.github.io/",
+  name: "Mohamed Shaheen",
+  role: "Frontend Developer",
+  linkedinId: "https://www.linkedin.com/in/mohamedshaheen26/",
+  skypeid: "Your skypeid",
+  roleDescription:
+    " I have a passion for technology and a keen eye for detail, which enables me to create clean and organized code that is easy to read and maintain. I am always eager to learn new things and stay up-to-date with the latest trends in frontend development. If you need a skilled developer to create an amazing Website, feel free to contact me.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/mohamedshaheen26/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/mohamedshaheen26",
+      className: "fa fa-github",
+    },
+    {
+      name: "skype",
+      url: "https://www.upwork.com/freelancers/~0155af2d3165d518b4?viewMode=1",
+      className: "fa fa-link",
+    },
+  ],
+  aboutme:
+    "UI Developer with 1+ years of experience in frontend development and user interface design, expert in HTML, CSS, and JavaScript. My key achievements include successfully developing user interface components for an ERP system, ensuring integration with backend functionalities for real-time data processing, and providing ongoing support based on user feedback. Seeking a UI/UX Developer position at [InnoTech], where I bring my frontend development and UI design skills to support your mission of enhancing user experiences and delivering high-quality software solutions.",
+  address: "Tanta, Egypt",
+  website: "",
+  education: [
+    {
+      UniversityName: "Tanta Unversity",
+      specialization: "Art",
+      MonthOfPassing: "Aug",
+      YearOfPassing: "2022",
+      Achievements: "",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "InnoTech Company",
+      specialization: "UI Developer",
+      MonthOfLeaving: "Oct",
+      YearOfLeaving: "2023",
+      Achievements: "",
+    },
+  ],
+  skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS3",
+    },
+    {
+      skillname: "JavaScript",
+    },
+    {
+      skillname: "TypeScript",
+    },
+    {
+      skillname: "Reactjs",
+    },
+  ],
+  portfolio: [
+    {
+      name: "project1",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+    {
+      name: "project2",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project.jpg",
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project2.png",
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+  ],
+};
+
+export default resumeData;
